0f80d055fa009e6e1e7fe1903e6570ad88512323
Rakesh Pandey <rakeshpandey@192.168.1.12>
2023-09-27 00:28:35 +0530

    Todo app setup

---
+++ 1.Todo/my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className='App-header'>
+          <h1>Todo App</h1>
+        </div>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
